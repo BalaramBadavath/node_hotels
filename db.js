3a1ea0407c7cfe386f769190ec9450c66810d1c8
Balaram <badavathbalaram17@gmail.com>
2024-09-09 17:27:12 +0530

    third commit

--- db.js
+++ db.js
@@ -1,9 +1,11 @@
+require('dotenv').config()
 const mongoose = require("mongoose")
 // const mongoURL = "mongodb://localhost:27017/hotels"
-require('dotenv').config()
 const mongoURL = process.env.MONGODB_URL
-
-mongoose.connect(mongoURL)
+mongoose.connect(mongoURL,{
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+})
 const db = mongoose.connection;
 
 db.on('connected', ()=>{console.log("Connected to MongoDB Server");})
--- server.js
+++ server.js
@@ -1,7 +1,7 @@
+require('dotenv').config()
 const express = require('express')
 const app = express()
 const db = require('./db')
-require('dotenv').config()
 
 const bodyParser = require('body-parser')
 app.use(bodyParser.json())
