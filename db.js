e5e1019b6c5d3a50e7abd5062a74ade3794965f6
Balaram <badavathbalaram17@gmail.com>
2024-09-09 17:11:00 +0530

    second commit

--- db.js
+++ db.js
@@ -1,7 +1,9 @@
 const mongoose = require("mongoose")
-const mongoURL = "mongodb://localhost:27017/hotels"
-mongoose.connect(mongoURL)
+// const mongoURL = "mongodb://localhost:27017/hotels"
+require('dotenv').config()
+const mongoURL = process.env.MONGODB_URL
 
+mongoose.connect(mongoURL)
 const db = mongoose.connection;
 
 db.on('connected', ()=>{console.log("Connected to MongoDB Server");})
